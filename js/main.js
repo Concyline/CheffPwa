a2532fb9e59bdc8d31f64c730bcf7c8bf5bbab99
AUREO\concy <concyinfo@gmail.com>
2026-03-04 14:17:28 -0300

    a

--- js/main.js
+++ js/main.js
@@ -11,14 +11,16 @@ function injectScript(path) {
 }
 
 //injectScript("https://code.jquery.com/jquery-3.6.0.min.js")
-injectScript("./js/topProgressBar.js")
+
 injectScript("./js/ready.js")
+
+injectScript("./js/storageManager.js")
+injectScript("./js/topProgressBar.js")
 injectScript("./js/componentes.js")
 injectScript("./js/menu_lateral.js")
 injectScript("./js/gerenteToken.js")
 injectScript("./js/serviceWorker.js")
-
 injectScript("./js/tokenManager.js")
 injectScript("./js/apiService.js")
-injectScript("./js/storageManager.js")
+
 
--- js/ready.js
+++ js/ready.js
@@ -30,28 +30,5 @@ $(function () {
         }, 5000); // corrigido: 500000 seria 8 minutos
     });
 
-    (async function () {
-        try {
-            const response = await fetch('./service-worker.js');
-
-            if (!response.ok) {
-                $('#versao').text(`Versão offline: ${StorageManager.get("versao")}`);
-                throw new Error('Não foi possível ler o service-worker.js');
-            }
-
-            const conteudo = await response.text();
-            const match = conteudo.match(/CACHE_NAME\s*=\s*['"`](.*?)['"`]/);
-
-            if (match && match[1]) {
-                $('#versao').text(`Versão: ${match[1]}`);
-                StorageManager.set("versao", match[1])
-            }
-
-        } catch (error) {
-            console.error('Erro:', error);
-            $('#versao').text(`Versão offline: ${StorageManager.get("versao")}`);
-        }
-    })();
-
 
 });
