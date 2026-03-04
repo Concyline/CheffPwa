af100039a12390edc12ca1df77d5459a102b981c
AUREO\concy <concyinfo@gmail.com>
2026-03-04 11:44:10 -0300

    a

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,44 @@
-const CACHE_NAME = 'meuapp-cache-v1.46';
+const CACHE_NAME = 'meuapp-cache-v1.47';
 
 const urlsToCache = [
     '/',                     // raiz
-    '/index.html',           // SPA principal
-    '/offline.html',         // página para quando estiver offline
-    '/manifest.json',
+    './index.html',           // SPA principal
+    './offline.html',         // página para quando estiver offline
+    './manifest.json',
 
-    '/img/avatar.png',
-    '/img/bedtime_24dp_49454F.png',
-    '/img/error_outline_72dp_49454F.png',
-    '/img/error_outline_72dp_CAC4D0.png',
-    '/img/favicon-32.png',
-    '/img/home_24dp_49454F.png',
-    '/img/home_24dp_CAC4D0.png',
-    '/img/icon-192.png',
-    '/img/icon-256.png',
-    '/img/icon-512.png',
-    '/img/icon-maskable-512.png',
-    '/img/settings_24dp_49454F.png',
-    '/img/settings_24dp_CAC4D0.png',
-    '/img/sunny_24dp_CAC4D0.png',
-    '/img/Untitled-2.fw.png',
+    './img/avatar.png',
+    './img/bedtime_24dp_49454F.png',
+    './img/error_outline_72dp_49454F.png',
+    './img/error_outline_72dp_CAC4D0.png',
+    './img/favicon-32.png',
+    './img/home_24dp_49454F.png',
+    './img/home_24dp_CAC4D0.png',
+    './img/icon-192.png',
+    './img/icon-256.png',
+    './img/icon-512.png',
+    './img/icon-maskable-512.png',
+    './img/settings_24dp_49454F.png',
+    './img/settings_24dp_CAC4D0.png',
+    './img/sunny_24dp_CAC4D0.png',
+    './img/Untitled-2.fw.png',
 
-    '/css/componentes.css',
-    '/css/geral.css',
-    '/css/loader.css',
-    '/css/menu_lateral.css',
-    '/css/top_bar.css',
-    '/css/variables.css',
+    './css/componentes.css',
+    './css/geral.css',
+    './css/loader.css',
+    './css/menu_lateral.css',
+    './css/top_bar.css',
+    './css/variables.css',
 
-    '/js/apiService.js',
-    '/js/componentes.js',
-    '/js/gerenteToken.js',
-    '/js/jquery-3.6.0.min.js',
-    '/js/main.js',
-    '/js/menu_lateral.js',
-    '/js/ready.js',
-    '/js/serviceWorker.js',
-    '/js/tokenManager.js',
-    '/js/topProgressBar.js'
+    './js/apiService.js',
+    './js/componentes.js',
+    './js/gerenteToken.js',
+    './js/jquery-3.6.0.min.js',
+    './js/main.js',
+    './js/menu_lateral.js',
+    './js/ready.js',
+    './js/serviceWorker.js',
+    './js/tokenManager.js',
+    './js/topProgressBar.js'
     // adicione outros arquivos importantes aqui (JS, CSS, fontes...)
 ];
 
