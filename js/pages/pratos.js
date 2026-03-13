dfe3697695ec8c54567ceb65725778d9f81665e2
AUREO\concy <concyinfo@gmail.com>
2026-03-13 11:46:45 -0300

    2026-03-13 11:46:45

--- js/pages/pratos.js
+++ js/pages/pratos.js
@@ -1,3 +1,31 @@
 $(function () {
 
 })
+
+async function notificar() {
+
+    console.log('NOTIFICAR')
+
+    if (!("Notification" in window)) {
+        alert("Este navegador não suporta notificações");
+        return;
+    }
+
+    let permissao = Notification.permission;
+
+    if (permissao === "default") {
+        permissao = await Notification.requestPermission();
+    }
+
+    if (permissao === "granted") {
+
+        new Notification("Novo pedido!", {
+            body: "Mesa 4 solicitou atendimento",
+            icon: "/img/icon.png"
+        });
+
+    } else {
+        console.log("Permissão negada");
+    }
+
+}
